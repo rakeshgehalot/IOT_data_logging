f687b51a9d584abb2ce7ca0d15d9dfbaa5821be4
rakeshgehalot <rakeshgehalotxd@gmail.com>
2017-07-01 10:16:53 +0530

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -3,7 +3,7 @@
 USER="rakesh"
 PASSWORD="pass"
 HOST="192.168.1.14"
-sshpass -p pass sftp -i /home/rakesh/Downloads/devappler.pem ec2-user@52.32.75.70 << EOF
+sshpass -p pass sftp -i /home/rakesh/Downloads/devappler.pem rt-user@00.00.00.00 << EOF
 put temperature /var/www/devappler
 quit 
 EOF
